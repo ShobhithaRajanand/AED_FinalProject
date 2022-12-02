84df0b03a8b6c5bc235787337c38bab788be1ff5
Aditi chikkali <chikkali.a@northeastern.edu>
2022-12-02 10:46:43 -0500

    Adding changes to Db40

---
+++ INFO5100_FinalProject/src/Business/DB4OUtil/DB4OUtil.java
@@ -0,0 +1,77 @@
+package Business.DB4OUtil;
+
+import Business.ConfigureSystem;
+import Business.EcoSystem;
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.config.EmbeddedConfiguration;
+import com.db4o.ta.TransparentPersistenceSupport;
+import java.nio.file.Paths;
+
+/**
+ *
+ * @author aditi
+ */
+public class DB4OUtil {
+
+    // path to the data store
+    private static final String FILENAME = Paths.get("Databank1.db4o").toAbsolutePath().toString();
+    private static DB4OUtil dB4OUtil;
+    
+    public synchronized static DB4OUtil getInstance(){
+        if (dB4OUtil == null){
+            dB4OUtil = new DB4OUtil();
+        }
+        return dB4OUtil;
+    }
+
+    protected synchronized static void shutdown(ObjectContainer conn) {
+        if (conn != null) {
+            conn.close();
+        }
+    }
+
+    private ObjectContainer createConnection() {
+        try {
+
+            EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
+            config.common().add(new TransparentPersistenceSupport());
+            //Controls the number of objects in memory
+            config.common().activationDepth(Integer.MAX_VALUE);
+            //Controls the depth/level of updation of Object
+            config.common().updateDepth(Integer.MAX_VALUE);
+            //Register your top most Class here
+            // Change to the object you want to save
+            config.common().objectClass(EcoSystem.class).cascadeOnUpdate(true); 
+            ObjectContainer db = Db4oEmbedded.openFile(config, FILENAME);
+            return db;
+        } catch (Exception ex) {
+            System.out.print(ex.getMessage());
+        }
+        return null;
+    }
+
+    public synchronized void storeSystem(EcoSystem system) {
+        ObjectContainer conn = createConnection();
+        conn.store(system);
+        conn.commit();
+        conn.close();
+    }
+    
+    public EcoSystem retrieveSystem(){
+        ObjectContainer conn = createConnection();
+        // Change to the object you want to retrive
+        ObjectSet<EcoSystem> systems = conn.query(EcoSystem.class); 
+        EcoSystem system;
+        if (systems.size() == 0){
+            // If there's no System in the record, create a new one
+            system = ConfigureSystem.configure();  
+        }
+        else{
+            system = systems.get(systems.size() - 1);
+        }
+        conn.close();
+        return system;
+    }
+}

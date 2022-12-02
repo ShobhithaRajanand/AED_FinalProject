c99d1f4fef2cf2764d45d40dc39b58dd0d65c753
Amitkumar Radheshyam Yadav <yadav.am@northeastern.edu>
2022-12-02 13:04:28 -0500

    Adding useraccount package in business

---
+++ INFO5100_FinalProject/src/Business/UserAccount/UserAccountDirectory.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.UserAccount;
+
+import Business.Employee.Employee;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author amityadav
+ */
+public class UserAccountDirectory {
+    
+   
+    
+    
+}

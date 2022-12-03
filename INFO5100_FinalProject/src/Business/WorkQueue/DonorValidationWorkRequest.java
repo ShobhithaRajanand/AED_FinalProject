166cffa98859fedb7b86e935ae2b43408099402d
Amitkumar Radheshyam Yadav <yadav.am@northeastern.edu>
2022-12-02 20:17:38 -0500

    Workqueue initial added in Business

---
+++ INFO5100_FinalProject/src/Business/WorkQueue/DonorValidationWorkRequest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.WorkQueue;
+
+import Business.Entity.Donor;
+
+/**
+ *
+ * @author amityadav
+ */
+public class DonorValidationWorkRequest extends WorkRequest{
+    
+    
+    
+    
+}
---
+++ INFO5100_FinalProject/src/Business/WorkQueue/WorkRequest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.WorkQueue;
+
+import Business.UserAccount.UserAccount;
+import java.util.Date;
+
+/**
+ *
+ * @author amityadav
+ */
+public abstract class WorkRequest {
+
+    
+
+}

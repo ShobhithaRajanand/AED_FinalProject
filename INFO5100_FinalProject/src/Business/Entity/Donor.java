d50bd33443997a35485965d70ce6209c1d65206f
Aditi chikkali <chikkali.a@northeastern.edu>
2022-12-03 22:29:32 -0500

    Adding changes to Donor

--- INFO5100_FinalProject/src/Business/Entity/Donor.java
+++ INFO5100_FinalProject/src/Business/Entity/Donor.java
@@ -13,6 +13,74 @@ import java.util.List;
  */
 public class Donor extends Person{
     
+    private String referenceName;
+    private String referenceNo;
+    private boolean isOrganAvailable;
+    private boolean isDonorFitForTransplant;
+    List<String> organList;
+    boolean isDocAssigned;
+    String docAssignedEmaild;
+
+    public String getDocAssignedEmaild() {
+        return docAssignedEmaild;
+    }
+
+    public void setDocAssignedEmaild(String docAssignedEmaild) {
+        this.docAssignedEmaild = docAssignedEmaild;
+    }
     
+    public boolean isIsDocAssigned() {
+        return isDocAssigned;
+    }
+
+    public void setIsDocAssigned(boolean isDocAssigned) {
+        this.isDocAssigned = isDocAssigned;
+    }
+
+    public String getReferenceName() {
+        return referenceName;
+    }
+
+    public void setReferenceName(String referenceName) {
+        this.referenceName = referenceName;
+    }
+
+    public String getReferenceNo() {
+        return referenceNo;
+    }
+
+    public void setReferenceNo(String referenceNo) {
+        this.referenceNo = referenceNo;
+    }
+
+    public boolean isIsOrganAvailable() {
+        return isOrganAvailable;
+    }
+
+    public void setIsOrganAvailable(boolean isOrganAvailable) {
+        this.isOrganAvailable = isOrganAvailable;
+    }
+
+    public List<String> getOrganList() {
+        return organList;
+    }
+
+    public void setOrganList(List<String> organList) {
+        this.organList = organList;
+    }
+    
+    public boolean isIsDonorFitForTransplant() {
+        return isDonorFitForTransplant;
+    }
+
+    public void setIsDonorFitForTransplant(boolean isDonorFitForTransplant) {
+        this.isDonorFitForTransplant = isDonorFitForTransplant;
+    }
+    
+
+    @Override
+    public String toString() {
+        return this.getPersonEmailId().trim();
+    }
 
 }

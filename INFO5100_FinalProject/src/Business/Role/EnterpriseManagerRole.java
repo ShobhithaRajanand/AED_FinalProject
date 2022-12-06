e21f01a7fd2d8f949bb658009c39141d0dde0723
Amitkumar Radheshyam Yadav <yadav.am@northeastern.edu>
2022-12-05 20:10:37 -0500

    EntManger role addition

--- INFO5100_FinalProject/src/Business/Role/EnterpriseManagerRole.java
+++ INFO5100_FinalProject/src/Business/Role/EnterpriseManagerRole.java
@@ -4,7 +4,12 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
 
+import Business.UserAccount.UserAccount;
+import UI.CommonUI.OrganizationAdminReusableWorkArea;
 import javax.swing.JPanel;
 
 /**
@@ -13,6 +18,15 @@ import javax.swing.JPanel;
  */
 public class EnterpriseManagerRole extends Role{
     
-   
+    RoleType roleType = RoleType.EnterpriseManagerRole;
     
+    @Override
+    public String toString() {
+        return this.roleType.getValue();
+    }
+    
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization org, Enterprise enterprise, EcoSystem business) {
+        return new OrganizationAdminReusableWorkArea(business, userProcessContainer, enterprise, account);
+    }
 }
